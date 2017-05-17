8bfcd1a0ba89a2a77de75eaffc50023ee971430e
yao <815092170@qq.com>
2017-05-18 01:15:59 +0800

    update

--- src/pages/coal-price-page/coal-price-page.ts
+++ src/pages/coal-price-page/coal-price-page.ts
@@ -14,11 +14,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CoalPricePage {
 
+  coalType: string = "mm";
+  coalList: any = [];
+  priceType: string = "coal";
+  queryText: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    let coal = {
+      store : "海湾煤矿",
+      hot: "5600",
+      coalPrice : "200",
+      salePrice : "500",
+      salerName : "张小花",
+      salerPhone : "13366633333"
+    }
+    this.coalList = [coal, coal, coal];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CoalPricePage');
   }
 
+  onCoalTypeChange() {
+    console.log(this.coalType);
+  }
+
+  changePriceType() {
+
+  }
+
+  doRefresh(refresher) {
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 4000);
+  }
+
+  doQuery(){
+    console.log(this.queryText);
+  }
 }
--- src/pages/person-page/person-page.ts
+++ src/pages/person-page/person-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import {LoginPage} from "../login-page/login-page";
 
 /**
@@ -15,7 +15,7 @@ import {LoginPage} from "../login-page/login-page";
 })
 export class PersonPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -27,6 +27,26 @@ export class PersonPage {
       name:"tom",
       job:"IT"
     })
+  }
+
+  logout() {
+    let alert = this.alertCtrl.create({
+      title: '温馨提示',
+      message: '你确定退出吗',
+      buttons: [
+        {
+          text: '确定',
+          handler: () => {
+            console.log('退出成功！');
+          }
+        },
+        {
+          text: '再看看',
+          role: 'cancel'
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
