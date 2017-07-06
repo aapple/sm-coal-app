69d6e146c53cad2124b319e56ec13e64f6ae129f
yao <815092170@qq.com>
2017-07-07 00:06:52 +0800

    update

--- src/modules/coal/pages/coal-price-detail.ts
+++ src/modules/coal/pages/coal-price-detail.ts
@@ -19,10 +19,10 @@ export class CoalPriceDetailPage {
     }
 
 
-    if(this.productPrice.priceOwnerType == 1){
-      this.priceType = "一票制";
-    } else {
+    if(this.productPrice.priceOwnerType == 2 || this.productPrice.factory.factoryType == 2){
       this.priceType = "两票制";
+    } else {
+      this.priceType = "一票制";
     }
   }
 
